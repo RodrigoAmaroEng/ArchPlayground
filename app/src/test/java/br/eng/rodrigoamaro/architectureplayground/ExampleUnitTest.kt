3eae3fa7d4ca11d0a0172e58bf39e312465becdd
Rodrigo Amaro <ramaro@uolinc.com>
2018-11-12 16:56:05 -0200

    - Android Studio 3.3 - First test automation

--- app/src/test/java/br/eng/rodrigoamaro/architectureplayground/ExampleUnitTest.kt
+++ app/src/test/java/br/eng/rodrigoamaro/architectureplayground/ExampleUnitTest.kt
@@ -1,59 +1,59 @@
 package br.eng.rodrigoamaro.architectureplayground
 
-import io.reactivex.Observable
+import android.view.View
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.experimental.delay
+import org.hamcrest.BaseMatcher
+import org.hamcrest.Description
 import org.junit.Test
-import java.util.*
+import org.junit.runner.RunWith
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
+
+@RunWith(AndroidJUnit4::class)
 class Fixtures {
+
     @Test
-    fun addition_isCorrect() {
-        val d = listOf(RandomManipulator(), OddEvenManipulator())
-        val m = listOf(SumManipulator(), MultiplyManipulator())
-        Observable.fromIterable(d)
-                .reduce(1) { v, f -> f.doStuff(v) }
-                .flatMap { v ->
-                    Observable.fromIterable(m)
-                            .reduce(v) { value, acc -> acc.doStuff(value) }
-                }
-                .doOnSuccess { println(it) }
-                .subscribe()
-    }
-
-    interface Manipulator {
-        fun doStuff(value: Int): Int
-    }
-
-    class SumManipulator : Manipulator {
-        override fun doStuff(value: Int): Int {
-            return value + 1
+    fun tryToBuySomeCoffee() {
+        ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.button_increase)).perform(ViewActions.click())
+        onView(withText("1")).check(isVisible())
+        onView(withText("Pagar")).perform(ViewActions.click())
+        suspend {
+            delay(5000)
+            onView(withText("Transação concluída!")).check(isVisible())
         }
+
     }
 
-    class MultiplyManipulator : Manipulator {
-        override fun doStuff(value: Int): Int {
-            return value * 2
-        }
-    }
-
-    class RandomManipulator : Manipulator {
-        override fun doStuff(value: Int): Int {
-            val nextInt = Random().nextInt(10)
-            println("Random $nextInt")
-            return value + nextInt
-        }
-    }
-
-    class OddEvenManipulator : Manipulator {
-        override fun doStuff(value: Int): Int {
-            return value % 2
-        }
-    }
-
+    private fun isVisible() = ViewAssertion { view, _ -> assertThat(view, VisibilityMatcher(View.VISIBLE)) }
 
 }
 
+private class VisibilityMatcher(private val visibility: Int) : BaseMatcher<View>() {
+
+    override fun describeTo(description: Description) {
+        val visibilityName: String = when (visibility) {
+            View.GONE -> "GONE"
+            View.VISIBLE -> "VISIBLE"
+            else -> "INVISIBLE"
+        }
+        description.appendText("View visibility must has equals $visibilityName")
+    }
+
+    override fun matches(o: Any?): Boolean {
+        if (o == null) {
+            if (visibility == View.GONE || visibility == View.INVISIBLE)
+                return true
+            else if (visibility == View.VISIBLE) return false
+        }
+
+        if (o !is View)
+            throw IllegalArgumentException("Object must be instance of View. Object is instance of " + o!!)
+        return o.visibility == visibility
+    }
+}
