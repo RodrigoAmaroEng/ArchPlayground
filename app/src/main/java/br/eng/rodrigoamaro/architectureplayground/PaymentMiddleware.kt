07f6b5079e7066f95573b963bebc642350900d2e
Rodrigo Amaro <ramaro@uolinc.com>
2018-11-13 18:51:29 -0200

    Coverage report + Native Coroutines

--- app/src/main/java/br/eng/rodrigoamaro/architectureplayground/PaymentMiddleware.kt
+++ app/src/main/java/br/eng/rodrigoamaro/architectureplayground/PaymentMiddleware.kt
@@ -3,12 +3,13 @@ package br.eng.rodrigoamaro.architectureplayground
 import br.eng.rodrigoamaro.architectureplayground.base.Action
 import br.eng.rodrigoamaro.architectureplayground.base.Middleware
 import br.eng.rodrigoamaro.architectureplayground.base.Store
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class PaymentMiddleware(private val service: PaymentService) : Middleware<SaleState> {
     override fun dispatch(action: Action, store: Store<SaleState>): Action {
         if (action == PayAction)
-            launch {
+            GlobalScope.launch {
                 try {
                     service.pay(store.initialState)
                     store.dispatch(PayCompletedAction)
