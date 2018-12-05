e21fb3bb3bd253519ac25d43571fb94aa0ddc665
Rodrigo Amaro <ramaro@uolinc.com>
2018-12-05 17:39:47 -0200

    Minor change

--- app/src/main/java/br/eng/rodrigoamaro/architectureplayground/coffee/PaymentService.kt
+++ app/src/main/java/br/eng/rodrigoamaro/architectureplayground/coffee/PaymentService.kt
@@ -9,10 +9,6 @@ interface PaymentService {
 class PaymentServiceImpl(private val api: Api) : PaymentService {
     override suspend fun pay(invoice: SaleState): Receipt? {
         val result = api.payForCoffee().await()
-        if (!result.isSuccessful) {
-            throw IllegalStateException()
-        } else {
-            return result.body()
-        }
+        if (!result.isSuccessful) throw IllegalStateException() else return result.body()
     }
 }
